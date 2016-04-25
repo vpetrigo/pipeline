f5d2329aaba1dc09583d1c93f55d05e7c04710b1
Vladimir Petrigo <taenaru@gmail.com>
2016-04-26 00:27:22 +0300

    add some error handling

--- upipe.cpp
+++ upipe.cpp
@@ -12,18 +12,28 @@
 
 #include "commands.hpp"
 
+class Pipe_Error : public std::system_error {
+ public:
+  using std::system_error::system_error;
+};
+
+class Process_Error : public std::system_error {
+ public:
+  using std::system_error::system_error;
+};
+
 std::array<int, 2> open_pipe() {
   constexpr int PIPE_FD = 2;
   std::array<int, 2> pipe_fd;
 
   if (pipe(pipe_fd.data()) != 0) {
-    throw std::system_error{std::error_code(errno, std::generic_category())};
+    throw Pipe_Error{std::error_code(errno, std::generic_category())};
   }
 
   return pipe_fd;
 }
 
-int call_command(const Command& cmd) {
+int call_command(const Command& cmd) noexcept {
   return (cmd.args.empty()) ? execlp(cmd.cmd.c_str(), cmd.cmd.c_str(), nullptr)
                             : execlp(cmd.cmd.c_str(), cmd.cmd.c_str(),
                                      cmd.args.c_str(), nullptr);
@@ -48,8 +58,7 @@ void spawn_proc(const Command& cmd, int& in_fd) {
       int retval = call_command(cmd);
 
       if (retval) {
-        throw std::system_error{
-            std::error_code(errno, std::generic_category())};
+        throw Process_Error{std::error_code(errno, std::generic_category())};
       }
     } 
     else {
@@ -58,8 +67,8 @@ void spawn_proc(const Command& cmd, int& in_fd) {
       wait(nullptr);
     }
   } 
-  catch (const std::system_error& se) {
-    std::cerr << "ERROR " << se.code() << ": " << se.what() << std::endl;
+  catch (const Pipe_Error& pe) {
+    std::cerr << "ERROR " << pe.code() << ": " << pe.what() << std::endl;
     throw;
   }
 }
@@ -69,7 +78,12 @@ void process_commands(const std::vector<Command>& cmds) {
   auto last_command = cmds.back();
 
   for (auto it = cmds.cbegin(); it != cmds.cend() - 1; ++it) {
-    spawn_proc(*it, in_fd);
+    try {
+      spawn_proc(*it, in_fd);
+    }
+    catch (const Pipe_Error& pe) {
+      throw;
+    }
   }
   // get data from the last pipe's out
   dup2(in_fd, STDIN_FILENO);
@@ -77,7 +91,7 @@ void process_commands(const std::vector<Command>& cmds) {
   int retval = call_command(last_command);
 
   if (retval) {
-    throw std::system_error{std::error_code(errno, std::generic_category())};
+    throw Process_Error{std::error_code(errno, std::generic_category())};
   }
 }
 
@@ -91,8 +105,9 @@ int main() {
   try {
       process_commands(cmds);
   }
-  catch (const std::system_error& se) {
-    std::cerr << "ERROR: " << se.code() << ": " << se.what() << std::endl;
+  catch (const std::system_error& pe) {
+    std::cerr << "ERROR " << pe.code() << ": " << pe.what() << std::endl;
+    
     return 1;
   }
 
